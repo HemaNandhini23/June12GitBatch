ca774de14bca1aa2068460f4a57b06224f6f38e5
HemaNandhini23 <hemanandhiiniee@gmail.com>
2023-06-12 16:09:48 +0530

    Karan done

--- src/org/abcd/Sample.java
+++ src/org/abcd/Sample.java
@@ -2,8 +2,12 @@ package org.abcd;
 
 public class Sample {
 
+	private void m2() {
+		System.out.println("Method 2 ----Karan");
+	}
+
 	public static void main(String[] args) {
-		
+
 		int a = 10;
 		System.out.println(a);
 	}
